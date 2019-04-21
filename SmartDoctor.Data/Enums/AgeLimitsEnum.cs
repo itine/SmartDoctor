b6308d4eaafa8e991c7bb42996dd591351facbdb
Sergey Alekseev <sergey.alekseev@altenar.com>
2019-04-21 17:25:20 +0300

    init

---
+++ SmartDoctor.Data/Enums/AgeLimitsEnum.cs
@@ -0,0 +1,15 @@
+﻿namespace SmartDoctor.Data.Enums
+{
+    public enum AgeLimitsEnum
+    {
+        FirstChildhood = 0,
+        SecondChildhood = 1,
+        Teenage = 2,
+        Youth = 3,
+        FirstMatureAge = 4,
+        SecondMatureAge = 5,
+        Elderly = 6,
+        Senile = 7,
+        LongLiver = 8
+    }
+}
---
+++ SmartDoctor.Data/Models/AgeLimit.cs
@@ -0,0 +1,37 @@
+﻿using SmartDoctor.Data.Enums;
+using System;
+
+namespace SmartDoctor.Data.Models
+{
+    public class AgeLimit
+    {
+        public byte Age { get; }
+        public AgeLimitsEnum Limit { get; }
+        public AgeLimit(byte age)
+        {
+            if (age <= 0)
+                throw new Exception($"Age value must be positive");
+            if (age < 4)
+                throw new Exception($"Not supported age");
+            else if (age >= 4 && age <= 7)
+                Limit = AgeLimitsEnum.FirstChildhood;
+            else if (age >= 8 && age <= 12)
+                Limit = AgeLimitsEnum.SecondChildhood;
+            else if (age >= 13 && age <= 16)
+                Limit = AgeLimitsEnum.Teenage;
+            else if (age >= 17 && age <= 21)
+                Limit = AgeLimitsEnum.Youth;
+            else if (age >= 22 && age <= 35)
+                Limit = AgeLimitsEnum.FirstMatureAge;
+            else if (age >= 36 && age <= 60)
+                Limit = AgeLimitsEnum.SecondMatureAge;
+            else if (age >= 61 && age <= 74)
+                Limit = AgeLimitsEnum.Elderly;
+            else if (age >= 75 && age <= 90)
+                Limit = AgeLimitsEnum.Senile;
+            else if (age >= 91)
+                Limit = AgeLimitsEnum.LongLiver;
+            Age = age;
+        }
+    }
+}
